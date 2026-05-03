6bb1c59e6c610caca5f55f6e96ea8dbc786e4fa3
Aflahalbj <aflahalbambangjaya@gmail.com>
2026-05-03 08:37:36 +0700

    edit db.js

--- db.js
+++ db.js
@@ -6,9 +6,15 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  // TAMBAHKAN PORT (Aiven pakai 25060)
+  port: process.env.DB_PORT || 16512,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  // WAJIB: Tambahkan SSL agar Aiven mau menerima koneksi
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
